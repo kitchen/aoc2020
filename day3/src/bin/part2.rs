d48ed153d1c5e5eb496838b55ed2256a688b1c8c
Jeremy Kitchen <kitchen@kitchen.io>
2020-12-07 11:54:09 -0800

    day3

---
+++ day3/src/bin/part2.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1]; // this will panic if there's no argument which is fine for this
+    let step_x: usize = args[2].parse().unwrap();
+    let step_y: usize = args[3].parse().unwrap();
+
+    let file = File::open(filename).expect("couldn't open file");
+
+    let mut trees = 0;
+    let mut y = 0;
+    let mut want_x = step_x;
+    let mut want_y = step_y;
+
+    for line in io::BufReader::new(file).lines() {
+        let line = line.unwrap();
+
+        let max_x = line.chars().count();
+        if y > 0 && y == want_y {
+            match line.chars().nth(want_x) {
+                Some('#') => trees += 1,
+                _ => {}
+            };
+
+            want_x += step_x;
+            if want_x >= max_x {
+                want_x -= max_x;
+            }
+
+            want_y += step_y;
+        }
+        y += 1;
+    }
+    println!("trees: {}", trees);
+}
